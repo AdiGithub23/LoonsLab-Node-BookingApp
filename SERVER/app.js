b76a29b37bfccf8e3c158ac23c09fdd0e8274ccf
Adheesha Gunathilake <gunathilakeadheesha23@gmail.com>
2024-07-19 23:57:07 +0530

    Update app.js - removed unwanted codes

--- SERVER/app.js
+++ SERVER/app.js
@@ -14,9 +14,6 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-// mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB connected successfully'))
-//   .catch(err => console.error('MongoDB connection error:', err));
 mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
